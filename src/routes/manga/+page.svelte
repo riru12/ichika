fabfc411e87dc77e21ec1cdd191e334f332b3564
riru12 <brilljulianrina@gmail.com>
2024-06-02 22:07:33 +0800

    genre addition through manga entry creation

--- src/routes/manga/+page.svelte
+++ src/routes/manga/+page.svelte
@@ -9,18 +9,28 @@
     <div class="manga-list">
         {#each data.mangaList.mangas as manga}
             <div class="flex flex-row gap-4">
-                <a href={`/manga/${manga._id}`}>
+                <a href={`/manga/${manga._id}`} class="max-h-48">
                     <CldImage width="500" height="750" src={`${manga._id}/${manga.coverImg}`} alt={`${manga.title} Cover`} class="rounded"/>
                 </a>
                 <div>  
                     <h2><a href={`/manga/${manga._id}`}>{manga.title}</a></h2>
                     <h3 class="text-neutral-600">{manga.author}</h3>
                     <p>{manga.desc}</p>
+                    <div class="flex flex-row gap-2 mt-4 flex-wrap">
+                        {#each manga.genres as genre}
+                            <div class="text-sm bg-neutral-800 px-3 py-1 rounded-xl">{genre}</div>
+                        {/each}
+                    </div>
                 </div>
             </div>
         {/each}
     </div>
     <div class="fixed right-10 bottom-10">
-        <a class="px-4 py-2 bg-[#9696c8d9] text-black rounded-3xl" href="/manga/new">Add New Manga</a>
+        <a class="px-4 py-2 bg-indigo-700 flex flex-row items-center gap-3 text-white hover:brightness-125 rounded-3xl" href="/manga/new">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+            </svg>
+            Add New Manga
+        </a>
     </div>
 </section>
--- src/routes/manga/new/+page.svelte
+++ src/routes/manga/new/+page.svelte
@@ -1,33 +1,75 @@
+<script lang="ts">
+    function genreHandler(){
+        const genreInput = document.getElementById('genre-input') as HTMLInputElement;
+        const inp = genreInput?.value;
+        if (inp){
+            const genreList = document.getElementById('genre-list');
+            const genreItemWrapper = document.createElement('div');
+            genreItemWrapper.classList.add('flex', 'flex-row', 'bg-neutral-600', 'px-1', 'py-1', 'rounded-xl')
+            
+            const genreItem = document.createElement('input');
+            genreItem.classList.add('text-sm', 'bg-neutral-600', 'text-neutral-300', 'text-center', 'pointer-events-none')
+            genreItem!.type = "text";
+            genreItem!.size = inp.length+1;
+            genreItem!.value = inp;
+            genreItem!.name = "genres[]";
+            genreItem!.readOnly = true;
+
+            const genreDel = document.createElement('button');
+            genreDel.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='currentColor' class='size-4'><path d='M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z' /></svg>"
+            genreDel.onclick = function () {
+                genreList?.removeChild(genreItemWrapper);
+            };
+
+            genreItemWrapper.appendChild(genreItem);
+            genreItemWrapper.appendChild(genreDel);
+            genreList?.appendChild(genreItemWrapper);
+            genreInput!.value = '';
+        }
+    }
+</script>
+
 <section class="mx-12 md:mx-24 xl:mx-60 2xl:mx-96 py-8">
     <form action="http://localhost:3000/api/manga/new" method="post" id="new-manga" enctype='multipart/form-data'>
         <div class="flex flex-col gap-4">
             <div class="flex flex-row gap-4 w-full flex-wrap">
                 <div class="flex flex-col gap-1 flex-grow">
                     <label for="title" class="text-sm ml-2">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Chainsaw Man" class="rounded-3xl px-4 py-1 bg-neutral-400 placeholder:text-neutral-600 text-black">
+                    <input type="text" id="title" name="title" placeholder="Chainsaw Man" class="rounded-3xl px-4 py-1 bg-[#13131a] text-white placeholder:text-neutral-600 ">
                 </div>
                 <div class="flex flex-col gap-1 flex-grow">
                     <label for="author" class="text-sm ml-2">Author</label>
-                    <input type="text" id="author" name="author" placeholder="Tatsuki Fujimoto" class="rounded-3xl px-4 py-1 bg-neutral-400 placeholder:text-neutral-600 text-black">
+                    <input type="text" id="author" name="author" placeholder="Tatsuki Fujimoto" class="rounded-3xl px-4 py-1 bg-[#13131a] text-white placeholder:text-neutral-600">
                 </div>
                 <div class="flex flex-col gap-1 flex-grow">
                     <label for="status" class="text-sm ml-2">Status</label>
-                    <select id="status" name="status" class="rounded-3xl px-4 py-1 bg-neutral-400 text-black">
+                    <select id="status" name="status" class="rounded-3xl px-4 py-1 bg-[#13131a] text-white">
                         <option value="Ongoing">Ongoing</option>
                         <option value ="Completed">Completed</option>
                     </select>
                 </div>
+            </div>
+            <div class="flex flex-col">
+                <label for="genres" class="text-sm ml-2">Genres</label>
+                <div id="genre-list" class="flex flex-row items-center gap-2">
+                    <input id="genre-input" type="text" placeholder="Enter a genre" class="rounded-3xl px-4 py-1 bg-[#13131a] text-white placeholder:text-neutral-600 w-min">
+                    <button on:click={genreHandler} type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
             </div>        
             <div>
                 <label for="desc" class="text-sm ml-2">Description</label>
-                <textarea id="desc" name="desc" class="w-full rounded-3xl px-4 py-1 bg-neutral-400 text-black h-36 resize-none"></textarea>
+                <textarea id="desc" name="desc" class="w-full rounded-3xl px-4 py-1 bg-[#13131a] text-white h-36 resize-none"></textarea>
             </div>
             <div class="flex flex-row gap-4">
                 <label for="cover">Upload the cover photo:</label> 
-                <input type="file" id="cover" name="cover" accept="image/png, image/jpeg" class="file:px-4 file:bg-neutral-400 file:text-black file:rounded-3xl file:cursor-pointer"/>
+                <input type="file" id="cover" name="cover" accept="image/png, image/jpeg" class="file:px-4 file:bg-neutral-400 file:text-black file:rounded-3xl file:cursor-pointer hover:file:brightness-125"/>
             </div>
             <div class="flex justify-end">
-                <button type="submit" form="new-manga" value="Submit" class="px-4 py-1 bg-[#9696c8d9] text-black rounded-3xl">Submit</button>
+                <button type="submit" form="new-manga" value="Submit" class="px-4 py-1 bg-indigo-700 text-white rounded-3xl hover:brightness-125">Submit</button>
             </div>
         </div>
     </form>
